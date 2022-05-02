7a39e3390f43a6d63311de86b9d706c4619819da
Murakami Masaya <masaya.murakami@arsaga.jp>
2022-05-02 19:55:02 +0900

    start creating navigation menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,56 +1,32 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-6xl mx-auto">
         <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+            <!-- Logo -->
+            <div class="shrink-0 flex items-center pl-5 sm:pl-10 lg:pl-20">
+                <div class="w-28 sm:w-40 md:w-48">
                     <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
                 </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
+            </div>
+            <div class="hidden sm:block my-auto pr-5 sm:pr-10 lg:pr-20">
+                <div>
+                    <a href="#" class="text-sm text-gray-700 dark:text-gray-500">お問い合わせ</a>
+                    <a href="#" class="ml-5 text-sm text-gray-700 dark:text-gray-500">マイページ</a>
+                </div>
+                <div class="">
+                    @auth
+                        <a href="{{ url('/logout') }}" class="lg:text-lg text-red-600 dark:text-red-500">ログアウト</a>
+                    @else
+                        <a href="{{ route('login') }}" class="lg:text-lg text-red-600 dark:text-red-500">ログイン</a>
+                        <a href="{{ route('register') }}" class="ml-5 lg:ml-10 lg:text-lg text-red-600 dark:text-red-500">新規登録</a>
+                    @endauth
                 </div>
             </div>
-
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
-            </div>
-
             <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+            <div class="flex items-center sm:hidden bg-red-500 px-3">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-white focus:outline-nonetransition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -67,26 +43,30 @@
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
+    </div>
 
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
+    <!-- Top Bar -->
+    <header class="bg-red-500 shadow">
+        <div class="mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <div class="max-w-6xl mx-auto flex justify-center sm:justify-between items-center">
+                <div class="flex">
+                    {{-- todo: inputにフォーカスした時、青い枠線が出現する --}}
+                    <form action="#" method="post" class="mr-2 lg:mr-5 w-32 md:w-40">
+                        <input class="bg-white appearance-none border-2 border-white rounded w-full py-2 px-2 md:px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-red-300"
+                        type="text" name="" value="会社を検索">
+                    </form>
+                    <form action="#" method="post" class="mr-2 w-32 md:w-40">
+                        <input class="bg-white appearance-none border-2 border-white rounded w-full py-2 px-2 md:px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-red-300"
+                        type="text" name="" value="スクールを検索">
+                    </form>
+                </div>
+                <div class="hidden sm:block">
+                    <a href="#" class="text-white dark:text-white text-sm lg:text-lg font-bold mr-2 lg:mr-5">企業情報</a>
+                    <a href="#" class="text-white dark:text-white text-sm lg:text-lg font-bold mr-2 lg:mr-5">スクール情報</a>
+                    <a href="#" class="text-white dark:text-white text-sm lg:text-lg font-bold mr-2 lg:mr-5">イベント情報</a>
+                    <a href="#" class="text-white dark:text-white text-sm lg:text-lg font-bold lg:mr-5">特集記事</a>
+                </div>
             </div>
         </div>
-    </div>
+    </header>
 </nav>
