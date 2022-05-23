54b54360383df86ee5c83cb2c1c55016791c6e34
Murakami Masaya <masaya.murakami@arsaga.jp>
2022-05-24 00:00:24 +0900

    TOP3のレイアウト調整

--- resources/views/components/company/top-three.blade.php
+++ resources/views/components/company/top-three.blade.php
@@ -1,9 +1,9 @@
-<div class="border-4 border-pink-500 rounded-md">
+<div class="border-4 border-pink-500 rounded-md mx-5 md:mx-0">
     <div class="py-2 bg-pink-500">
-        <div class="text-center text-white text-lg font-bold">
+        <div class="text-center text-white text-lg lg:text-xl font-bold">
             2022年最新
         </div>
-        <div class="text-center text-white text-lg font-bold">
+        <div class="text-center text-white text-lg lg:text-xl font-bold">
             企業ランキングTOP3
         </div>
     </div>
@@ -26,8 +26,8 @@
                 <div>
                     <div class="flex justify-between items-center mt-3 border-b border-gray-400">
                         <div class="flex items-center">
-                            <div class="w-10 mr-1 md:text-md lg:text-lg font-bold text-red-800">{{ $loop->index+1 . '位' }}</div>
-                            <div class="text-sm lg:text-md font-bold">
+                            <div class="w-10 mr-1 md:text-md lg:text-xl font-bold text-red-800">{{ $loop->index+1 . '位' }}</div>
+                            <div class="text-sm lg:text-lg font-bold">
                                 <a href="{{ route('company.detail',['company' => $company->id]) }}">
                                     {{ $company->name }}
                                 </a>
@@ -37,7 +37,7 @@
                             <div class="w-6 mr-1">
                                 <img src="{{ asset('images/GR.png') }}" alt="GR">
                             </div>
-                            <div class="text-md font-bold flex items-end">{{ $company->gr }}</div>
+                            <div class="text-md lg:text-lg font-bold flex items-end">{{ $company->gr }}</div>
                         </div>
                     </div>
                 </div>
--- resources/views/components/school/top-three.blade.php
+++ resources/views/components/school/top-three.blade.php
@@ -1,9 +1,9 @@
-<div class="border-4 border-orange-500 mb-5 rounded-md">
+<div class="border-4 border-orange-500 mb-5 rounded-md mx-5 md:mx-0">
     <div class="bg-orange-500 py-2">
-        <div class="text-center text-white text-lg font-bold">
+        <div class="text-center text-white text-lg lg:text-xl font-bold">
             2022年最新
         </div>
-        <div class="text-center text-white text-lg font-bold">
+        <div class="text-center text-white text-lg lg:text-xl font-bold">
             スクールランキングTOP3
         </div>
     </div>
@@ -26,8 +26,8 @@
                 <div>
                     <div class="flex justify-between items-center mt-3 border-b border-gray-400">
                         <div class="flex items-center">
-                            <div class="w-7 mr-1 md:text-md lg:text-lg font-bold text-red-800">{{ $loop->index+1 . '位' }}</div>
-                            <div class="text-sm lg:text-md font-bold">
+                            <div class="w-7 lg:w-10 mr-1 md:text-md lg:text-xl font-bold text-red-800">{{ $loop->index+1 . '位' }}</div>
+                            <div class="text-sm lg:text-lg font-bold">
                                 <a href="{{ route('school.detail', ['school' => $school->id]) }}">
                                     {{ $school->name }}
                                 </a>
@@ -37,7 +37,7 @@
                             <div class="w-6 mr-1">
                                 <img src="{{ asset('images/GR.png') }}" alt="GR">
                             </div>
-                            <div class="text-md font-bold flex items-end">{{ $school->gr }}</div>
+                            <div class="text-md lg:text-lg font-bold flex items-end">{{ $school->gr }}</div>
                         </div>
                     </div>
                 </div>
