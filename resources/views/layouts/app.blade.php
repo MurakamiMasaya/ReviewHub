8c33597d2e1aebd5899adeeb300547b1b532a9b1
Murakami Masaya <masaya.murakami@arsaga.jp>
2022-06-30 00:19:53 +0900

    サーチコンソールの導入

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="google-site-verification" content="LLQhqQHY18QguUtCc2bEz_PqejITZ4_d7BUtmy66xl4" />
 
         <title>{{ config('app.name', 'Laravel') }}</title>
         <link rel="icon" href="{{ asset('images/GR.png') }}">
