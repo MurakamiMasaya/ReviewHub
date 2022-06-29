02389f316a9559397971ab163dfdb964d7db7b64
Murakami Masaya <masaya.murakami@arsaga.jp>
2022-06-29 09:45:31 +0900

    スクールのテーブル情報を変更

--- database/migrations/2022_05_07_235008_create_schools_table.php
+++ database/migrations/2022_05_07_235008_create_schools_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('schools', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('address');
+            $table->string('address')->nullable();
             $table->bigInteger('phone')->unsigned()->nullable();
             $table->string('website_url');
             $table->timestamps();
--- resources/views/components/information.blade.php
+++ resources/views/components/information.blade.php
@@ -1,6 +1,8 @@
 <div>
     <div class="mt-3 text-md md:text-md font-bold">{{ $detail->name }}</div>
+    @if(isset($detail->address))
     <div class="mt-3 text-md md:text-md font-bold">住所：{{ $detail->address }}</div>
+    @endif
     <div class="mt-3 text-md md:text-md font-bold">Webサイト:{{ $detail->website_url }}</div>
     <div class="mt-10 text-md md:text-md font-bold border-b-2 border-gray-300 lg:flex">
         <div class="text-gray-600 ">{{ $detail->name}}の</div>
